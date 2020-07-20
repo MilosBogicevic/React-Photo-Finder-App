4347bfbea697bf3802e9616cb1eceaa13061521e
MilosBogicevic <milbog.development@gmail.com>
2020-07-20 18:53:12 +0200

    Axios fetch added

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,24 @@
 import React from "react";
+import axios from "axios";
 import Search from "./Search";
 
-const App = () => {
-  return (
-    <div className="ui container" style={{ marginTop: "10px" }}>
-      <Search />
-    </div>
-  );
-};
+class App extends React.Component {
+  onSearchSubmit(term) {
+    axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization: "Client-ID mLAYWnGqObxVSSx2xE_8noMZSKS1ZNcRxYfPg6kLJbE",
+      },
+    });
+  }
+
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: "10px" }}>
+        <Search onSubmit={this.onSearchSubmit} />
+      </div>
+    );
+  }
+}
 
 export default App;
