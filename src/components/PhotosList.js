f96e3763b87444c43ed99f1e05cb4685871c6fba
MilosBogicevic <milbog.development@gmail.com>
2020-07-23 20:07:55 +0200

    CSS Grid added.

--- src/components/PhotosList.js
+++ src/components/PhotosList.js
@@ -1,10 +1,11 @@
 import React from "react";
+import "./PhotosList.css";
 
 const PhotosList = (props) => {
   const photos = props.photos.map(({ description, id, urls }) => {
     return <img key={id} src={urls.regular} alt={description} />;
   });
-  return <div>{photos}</div>;
+  return <div className="photos-list">{photos}</div>;
 };
 
 export default PhotosList;
